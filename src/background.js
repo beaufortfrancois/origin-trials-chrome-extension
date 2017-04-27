044e18c0b3089f26f35c976997013c6acbbb5dbc
Francois Beaufort <beaufort.francois@gmail.com>
2017-04-27 10:03:55 +0200

    Handle Origin-Trial response header

--- src/background.js
+++ src/background.js
@@ -1,14 +1,42 @@
 // Show page action icon when origin trials token are detected.
 chrome.runtime.onMessage.addListener((message, sender) => {
-  
+
   if (message.numOriginTrialsTokens && message.numOriginTrialsTokens > 0) {
     chrome.pageAction.show(sender.tab.id);
-    
-    let title = 'An Origin Trials token has been found.';
+
+    let title = 'An Origin Trial token has been found.';
     if (message.numOriginTrialsTokens > 1) {
-      title = message.numOriginTrialsTokens + ' Origin Trials tokens have been found.';
+      title = message.numOriginTrialsTokens + ' Origin Trial tokens have been found.';
     }
-    
+
     chrome.pageAction.setTitle({ tabId: sender.tab.id, title: title });
   }
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeWebRequest.onRequest.removeRules(null, registerRules);
+});
+
+function registerRules() {
+  let rules = [{
+    conditions: [
+      new chrome.declarativeWebRequest.RequestMatcher(
+        {
+          stages: ['onHeadersReceived'],
+          responseHeaders: [{ nameEquals: 'Origin-Trial' }],
+        })
+    ],
+    actions: [
+      new chrome.declarativeWebRequest.SendMessageToExtension(
+        { message: 'WOOHOO' })
+    ],
+  }];
+  chrome.declarativeWebRequest.onRequest.addRules(rules);
+}
+
+chrome.declarativeWebRequest.onMessage.addListener(details => {
+  chrome.pageAction.show(details.tabId);
+
+  let title = 'An Origin Trial token has been found in a Response Header.';
+  chrome.pageAction.setTitle({ tabId: details.tabId, title: title });
 });
